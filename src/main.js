f62575a2ce71bedacd271b98ea811947a895bb3a
mtsiuniak <mtsuynyak1991@gmail.com>
2024-01-08 18:28:42 +0100

    чорновик сайту

--- src/main.js
+++ src/main.js
@@ -6,4 +6,54 @@ const onToggleMenuHandlerCL = () => menu.classList.toggle('is-open');
 buttonopen.addEventListener('click', onToggleMenuHandlerOP); 
 buttonclose.addEventListener('click', onToggleMenuHandlerCL);
 
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active')
+    })
+})
+
+
+
+// hero js
+
+const loadText = document.querySelector('.loading-title')
+const bg = document.querySelector('.hero-section')
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 60, 30, 0)}px)`
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
 
