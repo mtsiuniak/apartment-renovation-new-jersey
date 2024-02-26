671457b4ed731e54a96058c85175104d1573fb6f
mtsiuniak <53307866+mtsiuniak@users.noreply.github.com>
2024-02-26 20:43:22 +0000

    Deploying to gh-pages from @ mtsiuniak/apartment-renovation-new-jersey@eaf7b245156c1dd9dc070b860b56d300ee3badfe 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,12 +1,12 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{C as l}from"./assets/vendor-fab6ff31.js";const i="/apartment-renovation-new-jersey/assets/1-fe490b8e.jpg",o=[{original:i,description:"Hokkaido Flower"},{original:i,description:"Container Haulage Freight"},{original:i,description:"Aerial Beach View"},{original:"../img/gallery-pictures/IMG_3545.jpg",description:"Flower Blooms"},{original:"img/gallery-pictures/IMG_3546.jpg",description:"Alpine Mountains"},{original:"img/gallery-pictures/IMG_3547.jpg",description:"Mountain Lake Sailing"},{original:"img/gallery-pictures/IMG_3548.jpg",description:"Alpine Spring Meadows"},{original:"img/gallery-pictures/IMG_3549.jpg",description:"Nature Landscape"},{original:"img/gallery-pictures/IMG_3550.jpg",description:"Lighthouse Coast Sea"},{original:"img/gallery-pictures/IMG_3551.jpg",description:"Lighthouse Coast Sea"},{original:"img/gallery-pictures/IMG_3552.jpg",description:"Lighthouse Coast Sea"},{original:i,description:"Lighthouse Coast Sea"}],n=document.querySelector(".gallery"),t=o.map(({original:e,description:a})=>`
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{C as n}from"./assets/vendor-fab6ff31.js";const i="/apartment-renovation-new-jersey/assets/IMG_3542-3179e25a.jpg",r=[{original:i,description:"Hokkaido Flower"},{original:i,description:"Container Haulage Freight"},{original:i,description:"Aerial Beach View"},{original:i,description:"Flower Blooms"},{original:i,description:"Alpine Mountains"},{original:i,description:"Mountain Lake Sailing"},{original:i,description:"Alpine Spring Meadows"},{original:i,description:"Nature Landscape"},{original:i,description:"Lighthouse Coast Sea"},{original:i,description:"Lighthouse Coast Sea"},{original:i,description:"Lighthouse Coast Sea"},{original:i,description:"Lighthouse Coast Sea"}],t=document.querySelector(".gallery"),l=r.map(({original:a,description:e})=>`
     <li class="gallery-item">
-	<a class="gallery-link" data-fancybox="gallery" href=${e}>
+	<a class="gallery-link" data-fancybox="gallery" href=${a}>
 		<img 
 			class="gallery-image" 
-			src=${e} 
-			alt=${a}
+			src=${a} 
+			alt=${e}
 			/>
 	</a>
     </li>
-  `).join("");n.innerHTML=t;l.bind(".gallery a",{loop:!0,animationEffect:"fade",hideScrollbar:!1,Carousel:{transition:"slide"},caption:function(e,a){var r;return((r=a.thumbEl)==null?void 0:r.alt)||""}});
+  `).join("");t.innerHTML=l;n.bind("[data-fancybox]",{loop:!0,animationEffect:"fade",hideScrollbar:!1,Carousel:{transition:"slide"},caption:function(a,e){var o;return((o=e.thumbEl)==null?void 0:o.alt)||""}});
 //# sourceMappingURL=commonHelpers.js.map
