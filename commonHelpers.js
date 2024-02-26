a661ca2fa643664a6681282485ee9c1263087616
mtsiuniak <53307866+mtsiuniak@users.noreply.github.com>
2024-02-26 03:01:22 +0000

    Deploying to gh-pages from @ mtsiuniak/apartment-renovation-new-jersey@bca2339c35c0c7db16ad68b6d62c5e8c91931866 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as a}from"./assets/vendor-870f0eb5.js";new a(".gallery a",{captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as a}from"./assets/vendor-870f0eb5.js";new a(".gallery-item a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=commonHelpers.js.map
