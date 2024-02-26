ac361606daae9fb87a035dd056f144c6df780037
mtsiuniak <53307866+mtsiuniak@users.noreply.github.com>
2024-02-26 20:34:12 +0000

    Deploying to gh-pages from @ mtsiuniak/apartment-renovation-new-jersey@9adafc856f9833d6de9d989dad202f063612a9e5 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,12 +1,12 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{C as a}from"./assets/vendor-fab6ff31.js";const l="/apartment-renovation-new-jersey/assets/1-fe490b8e.jpg",o=[{original:l,description:"Hokkaido Flower"},{original:"./img/gallery-pictures/IMG_3543.jpg",description:"Container Haulage Freight"},{original:"/src/img/gallery-pictures/IMG_3544.jpg",description:"Aerial Beach View"},{original:"../img/gallery-pictures/IMG_3545.jpg",description:"Flower Blooms"},{original:"img/gallery-pictures/IMG_3546.jpg",description:"Alpine Mountains"},{original:"img/gallery-pictures/IMG_3547.jpg",description:"Mountain Lake Sailing"},{original:"img/gallery-pictures/IMG_3548.jpg",description:"Alpine Spring Meadows"},{original:"img/gallery-pictures/IMG_3549.jpg",description:"Nature Landscape"},{original:"img/gallery-pictures/IMG_3550.jpg",description:"Lighthouse Coast Sea"},{original:"img/gallery-pictures/IMG_3551.jpg",description:"Lighthouse Coast Sea"},{original:"img/gallery-pictures/IMG_3552.jpg",description:"Lighthouse Coast Sea"},{original:"img/gallery-pictures/IMG_3553.jpg",description:"Lighthouse Coast Sea"}],t=document.querySelector(".gallery"),g=o.map(({original:i,description:e})=>`
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{C as l}from"./assets/vendor-fab6ff31.js";const i="/apartment-renovation-new-jersey/assets/1-fe490b8e.jpg",o=[{original:i,description:"Hokkaido Flower"},{original:i,description:"Container Haulage Freight"},{original:i,description:"Aerial Beach View"},{original:"../img/gallery-pictures/IMG_3545.jpg",description:"Flower Blooms"},{original:"img/gallery-pictures/IMG_3546.jpg",description:"Alpine Mountains"},{original:"img/gallery-pictures/IMG_3547.jpg",description:"Mountain Lake Sailing"},{original:"img/gallery-pictures/IMG_3548.jpg",description:"Alpine Spring Meadows"},{original:"img/gallery-pictures/IMG_3549.jpg",description:"Nature Landscape"},{original:"img/gallery-pictures/IMG_3550.jpg",description:"Lighthouse Coast Sea"},{original:"img/gallery-pictures/IMG_3551.jpg",description:"Lighthouse Coast Sea"},{original:"img/gallery-pictures/IMG_3552.jpg",description:"Lighthouse Coast Sea"},{original:i,description:"Lighthouse Coast Sea"}],n=document.querySelector(".gallery"),t=o.map(({original:e,description:a})=>`
     <li class="gallery-item">
-	<a class="gallery-link" href=${i}>
+	<a class="gallery-link" data-fancybox="gallery" href=${e}>
 		<img 
 			class="gallery-image" 
-			src=${i} 
-			alt=${e}
+			src=${e} 
+			alt=${a}
 			/>
 	</a>
     </li>
-  `).join("");t.innerHTML=g;a.bind("[data-fancybox]",{loop:!0,animationEffect:"fade",hideScrollbar:!1,Carousel:{transition:"slide"},caption:function(i,e){var r;return((r=e.thumbEl)==null?void 0:r.alt)||""}});
+  `).join("");n.innerHTML=t;l.bind(".gallery a",{loop:!0,animationEffect:"fade",hideScrollbar:!1,Carousel:{transition:"slide"},caption:function(e,a){var r;return((r=a.thumbEl)==null?void 0:r.alt)||""}});
 //# sourceMappingURL=commonHelpers.js.map
