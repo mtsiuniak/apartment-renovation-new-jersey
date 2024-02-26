bca2339c35c0c7db16ad68b6d62c5e8c91931866
mtsiuniak <mtsuynyak1991@gmail.com>
2024-02-25 22:01:00 -0500

    test!

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,68 +1,68 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const images = [
-  {
-    original:
-      "../img/gallery-pictures/IMG_3542.jpg",
-    description: 'Hokkaido Flower',
-  },
-  {
-    original:
-     "./img/gallery-pictures/IMG_3543.jpg",
-    description: 'Container Haulage Freight',
-  },
-  {
-    original:
-      "/src/img/gallery-pictures/IMG_3544.jpg",
-    description: 'Aerial Beach View',
-  },
-  {
-    original:
-      "../img/gallery-pictures/IMG_3545.jpg",
-    description: 'Flower Blooms',
-  },
-  {
-    original:
-      'img/gallery-pictures/IMG_3546.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    original:
-     'img/gallery-pictures/IMG_3547.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    original:
-      'img/gallery-pictures/IMG_3548.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    original:
-      'img/gallery-pictures/IMG_3549.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    original:
-      'img/gallery-pictures/IMG_3550.jpg',
-    description: 'Lighthouse Coast Sea',
-    },
-  {
-    original:
-      'img/gallery-pictures/IMG_3551.jpg',
-    description: 'Lighthouse Coast Sea',
-    },
-  {
-    original:
-      'img/gallery-pictures/IMG_3552.jpg',
-    description: 'Lighthouse Coast Sea',
-    },
-  {
-    original:
-      'img/gallery-pictures/IMG_3553.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
+// const images = [
+//   {
+//     original:
+//       "../img/gallery-pictures/IMG_3542.jpg",
+//     description: 'Hokkaido Flower',
+//   },
+//   {
+//     original:
+//      "./img/gallery-pictures/IMG_3543.jpg",
+//     description: 'Container Haulage Freight',
+//   },
+//   {
+//     original:
+//       "/src/img/gallery-pictures/IMG_3544.jpg",
+//     description: 'Aerial Beach View',
+//   },
+//   {
+//     original:
+//       "../img/gallery-pictures/IMG_3545.jpg",
+//     description: 'Flower Blooms',
+//   },
+//   {
+//     original:
+//       'img/gallery-pictures/IMG_3546.jpg',
+//     description: 'Alpine Mountains',
+//   },
+//   {
+//     original:
+//      'img/gallery-pictures/IMG_3547.jpg',
+//     description: 'Mountain Lake Sailing',
+//   },
+//   {
+//     original:
+//       'img/gallery-pictures/IMG_3548.jpg',
+//     description: 'Alpine Spring Meadows',
+//   },
+//   {
+//     original:
+//       'img/gallery-pictures/IMG_3549.jpg',
+//     description: 'Nature Landscape',
+//   },
+//   {
+//     original:
+//       'img/gallery-pictures/IMG_3550.jpg',
+//     description: 'Lighthouse Coast Sea',
+//     },
+//   {
+//     original:
+//       'img/gallery-pictures/IMG_3551.jpg',
+//     description: 'Lighthouse Coast Sea',
+//     },
+//   {
+//     original:
+//       'img/gallery-pictures/IMG_3552.jpg',
+//     description: 'Lighthouse Coast Sea',
+//     },
+//   {
+//     original:
+//       'img/gallery-pictures/IMG_3553.jpg',
+//     description: 'Lighthouse Coast Sea',
+//   },
+// ];
 
 // const galleryContainer = document.querySelector('.gallery');
 
@@ -82,7 +82,7 @@ const images = [
 
 // galleryContainer.innerHTML = galleryItemsHTML;
 
-let gallery = new SimpleLightbox('.gallery a', {
+let gallery = new SimpleLightbox('.gallery-item a', {
   captionsData: 'alt',
   captionDelay: 250
 });
