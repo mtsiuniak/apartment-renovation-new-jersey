fcf1bffd443fcbf581c84ffe15c810d99aca01d0
mtsiuniak <mtsuynyak1991@gmail.com>
2024-02-26 15:27:12 -0500

    test import

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,6 +3,10 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
+import image123 from "../img/large-pictures/1.jpg"
+
+
+
         // let gallery = new SimpleLightbox('.gallery-item a', {
         //     captionsData: 'alt',
         //     captionDelay: 250
@@ -11,7 +15,89 @@ import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 
 
-    Fancybox.bind("[data-fancybox]", {
+    
+const images = [
+  {
+    original:
+      image123,
+    description: 'Hokkaido Flower',
+  },
+  {
+    original:
+     "./img/gallery-pictures/IMG_3543.jpg",
+    description: 'Container Haulage Freight',
+  },
+  {
+    original:
+      "/src/img/gallery-pictures/IMG_3544.jpg",
+    description: 'Aerial Beach View',
+  },
+  {
+    original:
+      "../img/gallery-pictures/IMG_3545.jpg",
+    description: 'Flower Blooms',
+  },
+  {
+    original:
+      'img/gallery-pictures/IMG_3546.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    original:
+     'img/gallery-pictures/IMG_3547.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    original:
+      'img/gallery-pictures/IMG_3548.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    original:
+      'img/gallery-pictures/IMG_3549.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    original:
+      'img/gallery-pictures/IMG_3550.jpg',
+    description: 'Lighthouse Coast Sea',
+    },
+  {
+    original:
+      'img/gallery-pictures/IMG_3551.jpg',
+    description: 'Lighthouse Coast Sea',
+    },
+  {
+    original:
+      'img/gallery-pictures/IMG_3552.jpg',
+    description: 'Lighthouse Coast Sea',
+    },
+  {
+    original:
+      'img/gallery-pictures/IMG_3553.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsHTML = images.map(({ original, description }) => {
+  return `
+    <li class="gallery-item">
+	<a class="gallery-link" href=${original}>
+		<img 
+			class="gallery-image" 
+			src=${original} 
+			alt=${description}
+			/>
+	</a>
+    </li>
+  `;
+}).join('');
+
+galleryContainer.innerHTML = galleryItemsHTML;
+
+Fancybox.bind("[data-fancybox]", {
         loop: true,
         animationEffect: "fade",
         hideScrollbar: false,
@@ -24,86 +110,6 @@ import "@fancyapps/ui/dist/fancybox/fancybox.css";
     }
     )
 
-// const images = [
-//   {
-//     original:
-//       "../img/gallery-pictures/IMG_3542.jpg",
-//     description: 'Hokkaido Flower',
-//   },
-//   {
-//     original:
-//      "./img/gallery-pictures/IMG_3543.jpg",
-//     description: 'Container Haulage Freight',
-//   },
-//   {
-//     original:
-//       "/src/img/gallery-pictures/IMG_3544.jpg",
-//     description: 'Aerial Beach View',
-//   },
-//   {
-//     original:
-//       "../img/gallery-pictures/IMG_3545.jpg",
-//     description: 'Flower Blooms',
-//   },
-//   {
-//     original:
-//       'img/gallery-pictures/IMG_3546.jpg',
-//     description: 'Alpine Mountains',
-//   },
-//   {
-//     original:
-//      'img/gallery-pictures/IMG_3547.jpg',
-//     description: 'Mountain Lake Sailing',
-//   },
-//   {
-//     original:
-//       'img/gallery-pictures/IMG_3548.jpg',
-//     description: 'Alpine Spring Meadows',
-//   },
-//   {
-//     original:
-//       'img/gallery-pictures/IMG_3549.jpg',
-//     description: 'Nature Landscape',
-//   },
-//   {
-//     original:
-//       'img/gallery-pictures/IMG_3550.jpg',
-//     description: 'Lighthouse Coast Sea',
-//     },
-//   {
-//     original:
-//       'img/gallery-pictures/IMG_3551.jpg',
-//     description: 'Lighthouse Coast Sea',
-//     },
-//   {
-//     original:
-//       'img/gallery-pictures/IMG_3552.jpg',
-//     description: 'Lighthouse Coast Sea',
-//     },
-//   {
-//     original:
-//       'img/gallery-pictures/IMG_3553.jpg',
-//     description: 'Lighthouse Coast Sea',
-//   },
-// ];
-
-// const galleryContainer = document.querySelector('.gallery');
-
-// const galleryItemsHTML = images.map(({ original, description }) => {
-//   return `
-//     <li class="gallery-item">
-// 	<a class="gallery-link" href=${original}>
-// 		<img 
-// 			class="gallery-image" 
-// 			src=${original} 
-// 			alt=${description}
-// 			/>
-// 	</a>
-//     </li>
-//   `;
-// }).join('');
-
-// galleryContainer.innerHTML = galleryItemsHTML;
 
 // let gallery = new SimpleLightbox('.gallery-item a', {
 //   captionsData: 'alt',
