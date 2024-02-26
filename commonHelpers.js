5faa09d5cf8176bb6fdfbfdaeb7dc5d8ea9e3d00
mtsiuniak <53307866+mtsiuniak@users.noreply.github.com>
2024-02-26 04:48:09 +0000

    Deploying to gh-pages from @ mtsiuniak/apartment-renovation-new-jersey@a21d247bf1343428d5ac93aca2f5badc71ee7cfc 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as a}from"./assets/vendor-870f0eb5.js";new a(".gallery-item a",{captionsData:"alt",captionDelay:250});
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{C as t}from"./assets/vendor-fab6ff31.js";t.bind("[data-fancybox]",{loop:!0,animationEffect:"fade",hideScrollbar:!1,Carousel:{transition:"slide"},caption:function(n,o){var a;return((a=o.thumbEl)==null?void 0:a.alt)||""}});
 //# sourceMappingURL=commonHelpers.js.map
