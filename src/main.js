d91cc4fcf6eeaeb977510fc653bd3bf699eaf21c
mtsiuniak <mtsuynyak1991@gmail.com>
2024-02-29 20:46:29 -0500

    add scroll top btn

--- src/main.js
+++ src/main.js
@@ -72,5 +72,23 @@ for (let smoothLink of smoothLinks) {
     });
 };
 
+// scroll top button -----
 
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
+window.addEventListener("scroll", () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+    // Значення 'document.documentElement.scrollTop' вказує на прокрутку сторінки вгору
+    // 'behavior: "smooth"' забезпечує плавну анімацію прокрутки
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
